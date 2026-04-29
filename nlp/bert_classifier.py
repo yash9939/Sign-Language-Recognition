868d68f49edf6b89ed1375086acca931cf5db03e
Shivam Kumar <shivam9142kumar@gmail.com>
2026-04-29 10:59:31 +0530

    Removed unwanted files

--- nlp/bert_classifier.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("text-classification")
-
-def check(sentence):
-    return classifier(sentence)
--- nlp/ngram_model.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from collections import Counter
-
-def build_ngram(words, n=2):
-    ngrams = []
-    for word in words:
-        ngrams.extend(list(nltk.ngrams(word, n)))
-    return Counter(ngrams)
